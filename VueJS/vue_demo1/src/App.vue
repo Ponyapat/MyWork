15a294ba73196c55d203dc14e4bb877e9d0be054
Guide <poonyapat_anusonthi@cmu.ac.th>
2021-06-02 23:52:38 +0700

    vue_demo1

---
+++ VueJS/vue_demo1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>v-model</h1>
+    <!-- MVVM -->
+    <input v-model="message" type="text" placeholder="Enter your text">
+    <div>
+      {{message}}
+    </div>
+
+    <button @click="message=''">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      message:""
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>

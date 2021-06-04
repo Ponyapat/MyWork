979b928dfde4acca0c142f8b72ae82c2f29a2737
Guide <poonyapat_anusonthi@cmu.ac.th>
2021-06-04 22:23:03 +0700

    update

--- VueJS/vue_demo1/src/views/Demo3_Data.vue
+++ VueJS/vue_demo1/src/views/Demo3_Data.vue
@@ -7,6 +7,12 @@
       <div>{{isLogged}} </div>
       <input type="text" placeholder="Enter Text" v-model="message">
       <h2 v-if="isLogged">Yahoo</h2>
+      <div>{{account.username}} {{account.password}} </div>
+      <div>{{course[0]}} </div>
+
+      <ul>
+          <li v-for="item in course" :key="item.course">{{index}}.{{item}}</li>
+      </ul>
   </div>
 </template>
 
@@ -20,12 +26,27 @@ export default {
         return {
             count: 0,
             message: "",
-            isLogged: false
+            isLogged: false,
+            account: {
+                username: "admin",
+                password: "1234"
+            },
+        course: [
+            "Vue",
+            "React",
+            "Flutter",
+            "56789"
+        ]
         }
     },
 }
 </script>
 
-<style>
+<style scoped>
+
+    li{
+        color: red;
+
+    }
 
 </style>

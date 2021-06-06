09770a5f7e46a1a7cf13d5fca3ba77d8dae17622
Guide <poonyapat_anusonthi@cmu.ac.th>
2021-06-06 23:41:56 +0700

    vue_slot

---
+++ VueJS/vue_demo1/src/components/helper/Clock.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="clock">
+     <slot></slot>: {{time}}
+  </div>
+</template>
+
+<script>
+
+import moment from "moment";
+
+export default {
+    name: "Clock",
+    data() {
+        return {
+            time: ""
+        }
+    },
+    mounted() {
+        setInterval(()=>{
+            this.time = moment(Date()).format('MM/DD/YYY hh:mm:ss')
+        }, 1000)
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ VueJS/vue_demo1/src/views/Demo7_Slot.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="demo7">
+      <Clock>
+          <span style="color:red">เวลาปัจจุบัน</span>
+      </Clock>
+
+      <Clock>
+          <span style="color:Blue">Current Time</span>
+      </Clock>
+  </div>
+</template>
+
+<script>
+import Clock from "@/components/helper/Clock.vue";
+export default {
+    name: "Demo7",
+    components: {
+        Clock
+    }
+}
+</script>
+
+<style>
+
+</style>

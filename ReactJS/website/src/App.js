5aeb002badac29a5244b46f7e9e107d99128a95c
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-10-15 18:05:29 +0700

    bootstap

--- ReactJS/website/src/App.js
+++ ReactJS/website/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
 
 function App() {
+
   return (
     <div className="App">
-      <Header title = "ThaiLand"/>
-
-      <h1>Hello <i>React</i></h1>
+      <div class="alert-primary">alert-primary</div>
+      <div class="alert-secondary">alert-second</div>
+      <div class="alert-dark">alert-dark</div>
+      <div class="alert-light">alert-light</div>
+      <div class="alert-info">alert-info</div>
+      <div class="alert-success">alert-success</div>
+      <div class="alert-warning">alert-warning</div>
+      <div class="alert-danger">alert-danger</div>
     </div>
+
   );
 }
 

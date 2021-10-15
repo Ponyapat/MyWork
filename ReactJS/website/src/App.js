f78f432038694bed1f2ba504932db784fdc47476
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-10-15 20:18:48 +0700

    btn

--- ReactJS/website/src/App.js
+++ ReactJS/website/src/App.js
@@ -4,17 +4,33 @@ import './App.css';
 function App() {
 
   return (
-    <div className="App">
-      <div class="alert-primary">alert-primary</div>
-      <div class="alert-secondary">alert-second</div>
-      <div class="alert-dark">alert-dark</div>
-      <div class="alert-light">alert-light</div>
-      <div class="alert-info">alert-info</div>
-      <div class="alert-success">alert-success</div>
-      <div class="alert-warning">alert-warning</div>
-      <div class="alert-danger">alert-danger</div>
-    </div>
+    
+    <div class="MenuBarTextAlign">
+     <button type="button">normal</button>
+      <button type="button" class="btn">btn</button>
+      <button type="button" class="btn-primary">btn-primary</button>
+      <button type="button" class="btn-secondary">btn-secondary</button>
+      <hr />
+      <button type="button" class="btn-info">btn-ibfo</button>
+      <button type="button" class="btn-warning">btn-warning</button>
+      <button type="button" class="btn-danger">btn-danger</button>
+      <button type="button" class="btn-success">btn-success</button>
+      <hr />
+      <button type="button" class="btn-dark">btn-dark</button>
+      <button type="button" class="btn-light">btn-light</button>
+      <button type="button" class="btn-link">btn-link</button>
+      <hr />
 
+      <div class="btn-group">
+        <button class="btn">Menu</button>
+        <button class="btn">Webboard</button>
+        <button class="btn">Post</button>
+        <button class="btn">Contact</button>
+
+      </div>
+    
+    </div>
+  
   );
 }
 

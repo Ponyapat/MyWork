f67fe353d325ae922ddde31ccd2b471cf9e64530
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-10-15 15:26:28 +0700

    new

---
+++ ReactJS/website/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title = "ThaiLand"/>
+
+      <h1>Hello <i>React</i></h1>
+    </div>
+  );
+}
+
+export default App;

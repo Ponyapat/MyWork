5213aaf0d4d1a942422faae14c562940ce29f749
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-10-19 19:40:04 +0700

    redux

--- ReactJS/website/src/index.js
+++ ReactJS/website/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,46 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const reducer=(state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUB":
+      state = state - action.payload;
+      break;
+      default:
+      return state;
+  }
+  return state;
+
+}
+
+const store = createStore(reducer, 100, window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__());
+  store.subscribe(()=>{
+    console.log("Current Value : ", store.getState());
+  });
+
+
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 5
+});
+
+store.dispatch({
+  type: "SUB",
+  payload: 50
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 20
+});

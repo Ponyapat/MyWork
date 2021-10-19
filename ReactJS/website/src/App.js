3c707c24ab7b31ed541e6b6dd933148f78072db0
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-10-19 18:33:53 +0700

    axios select

--- ReactJS/website/src/App.js
+++ ReactJS/website/src/App.js
@@ -2,31 +2,45 @@ import React , {useState,useEffect} from 'react';
 import Axios from 'axios';
 
 function App() {
-  const [data, setdata] = useState([]);
+  const [data, setData] = useState({});
+  const [id, setId] = useState(1);
+  const [currentid, setcurrentid] = useState(1);
+
+  function showData() {
+    setcurrentid(id)
+  }
 
 
   useEffect(() => {
-    Axios.get('https://jsonplaceholder.typicode.com/posts')
+    Axios.get('https://jsonplaceholder.typicode.com/posts/' + id )
     .then(res=>{
       console.log(res)
-      setdata(res.data)
+      setData(res.data)
     })
     .catch(err=>{
       console.log(err)
     });
-  }, [])
+  }, [currentid])
 
     return (
       <div>
+
+        ID : <input type="text" value={id} 
+        onChange={e=>setId(e.target.value)}/>
+        <button type="button" onClick={showData}>Read Data</button>
+        <br />
         <table border='1'>
           <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Detail</th>
           </tr>
-          {
-            data.map(i=>(<tr><td>{i.id}</td><td>{i.title}</td><td>{i.body}</td></tr> ))
-          }
+          <tr>
+            <td>{data.id}</td>
+            <td>{data.title}</td>
+            <td>{data.body}</td>
+          </tr>
+
         </table>
       </div>
     );

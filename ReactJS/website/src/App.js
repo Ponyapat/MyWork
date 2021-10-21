2520eb8a3431f6078a13deaa20e5ff6315e1ba7b
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-10-21 16:42:17 +0700

    state dispatch

--- ReactJS/website/src/App.js
+++ ReactJS/website/src/App.js
@@ -1,12 +1,15 @@
 import React , {useReducer} from 'react';
 
-const iniState = 0;
+const iniState = {
+  myData : 0
+}
+
 const reducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case 'ADD':
-      return state + 1
+      return {myData: state.myData + action.payload}
     case 'SUB':
-      return state - 1
+      return {myData: state.myData - action.payload}
     case 'CLEAR' : 
       return iniState
       default:
@@ -20,10 +23,10 @@ const [data, dispatch] = useReducer(reducer, iniState)
   return (
   <div>
     <h1>App Component</h1>
-    <div>Value : {data} </div>
-      <button onClick={()=>dispatch('ADD')}>+</button>
-      <button onClick={()=>dispatch('SUB')}>-</button>
-      <button onClick={()=>dispatch('CLEAR')}>Clear</button>
+    <div>Value : {data.myData} </div>
+      <button onClick={()=>dispatch({type: 'ADD', payload:5})}>+</button>
+      <button onClick={()=>dispatch({type: 'SUB', payload:5})}>-</button>
+      <button onClick={()=>dispatch({type: 'CLEAR'})}>Clear</button>
 
   </div>
 );

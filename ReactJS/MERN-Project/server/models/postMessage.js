52ca49edcc82528a9dbc37635e0b6007b2bd3f0b
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-11-01 18:34:10 +0700

    component

--- ReactJS/MERN-Project/server/models/postMessage.js
+++ ReactJS/MERN-Project/server/models/postMessage.js
@@ -4,7 +4,7 @@ const postSchema = mongoose.Schema({
     title: String,
     message: String,
     creator: String,
-    tag: [string],
+    tag: [String],
     selectedFile: String,
     likeCount: {
         type: Number,
--- ReactJS/MERN-Project/server/routes/posts.js
+++ ReactJS/MERN-Project/server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getPosts} from '../comtrollers/posts.js'
+import { getPosts, createPost} from '../comtrollers/posts.js'
 
 const router = express.Router();
 
 //localhost:5000/posts
 router.get('/' , getPosts);
+router.post('/', createPost);
+
 
 export default router;

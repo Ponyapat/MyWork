96ae82cb6f4bf944fbaab5b4eb2dd25f4b75f99a
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-11-02 01:23:40 +0700

    create post

--- ReactJS/MERN-Project/server/index.js
+++ ReactJS/MERN-Project/server/index.js
@@ -3,7 +3,8 @@ import bodyParser  from 'body-parser';
 import  mongoose  from 'mongoose';
 import cors from 'cors';
 
-import postRoutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
+
 
 const app = express();
 
@@ -11,7 +12,9 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb" , extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
+
 app.use('/posts', postRoutes);
 
 

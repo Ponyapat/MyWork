f2fe6945e0f5af085b5e333efe398828e1231f51
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-11-07 14:37:05 +0700

    like

--- ReactJS/MERN-Project/client/src/components/Posts/Post/Post.js
+++ ReactJS/MERN-Project/client/src/components/Posts/Post/Post.js
@@ -5,11 +5,15 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 import useStyles from './styles';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     
     return (
        
@@ -58,7 +62,7 @@ const Post = ({ post, setCurrentId }) => {
                     {post.likeCount}
                 </Button>
 
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize='small' />
                     Delete
                 </Button>
